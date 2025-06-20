7c0244d9ea525ae0393efd3be36baed50ddf7a0e
Hikmah778 <raudatulhikmah342@gmail.com>
2025-06-21 04:48:15 +0800

    Update Raudatul_hikmah.java
    Menampilkan FrameApp melalu file Raudatul_hikmah.java

--- src/raudatul_hikmah/Raudatul_hikmah.java
+++ src/raudatul_hikmah/Raudatul_hikmah.java
@@ -14,7 +14,8 @@ public class Raudatul_hikmah {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FrameApp app = new FrameApp();
+        app.setVisible(true);
     }
     
 }
